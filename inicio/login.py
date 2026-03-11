0873bbf431998f2223b0552a98eafe5bf456dac4
Alan2527 <98465440+Alan2527@users.noreply.github.com>
2026-03-11 10:40:55 -0300

    Update login.py

--- inicio/login.py
+++ inicio/login.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@allure.feature("Frontend AMV Travel")
+@allure.feature("Login")
 @allure.story("Login de usuario")
 def test_login_amv(driver):
     wait = WebDriverWait(driver, 15)
