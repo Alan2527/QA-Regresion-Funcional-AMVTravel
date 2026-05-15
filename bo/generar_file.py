e8e48336cd999034ba1e93cf539a164bbf63faea
Alan2527 <98465440+Alan2527@users.noreply.github.com>
2026-05-15 09:15:54 -0300

    Update generar_file.py

--- bo/generar_file.py
+++ bo/generar_file.py
@@ -116,3 +116,9 @@ def test_generar_file_desde_inbox(driver):
         driver.execute_script("arguments[0].scrollIntoView({block:'center'});", tabla_totales)
         assert tabla_totales.is_displayed(), "La tabla de Totales no se renderizó correctamente."
         allure.attach(driver.get_screenshot_as_png(), name="Tabla_Totales_OK", attachment_type=allure.attachment_type.PNG)
+
+        # 12. Validar tabla Totales
+        tabla_costos = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@id='updCostBySupplier']//table[contains(@class, 'table-striped')]")))
+        driver.execute_script("arguments[0].scrollIntoView({block:'center'});", tabla_costos)
+        assert tabla_totales.is_displayed(), "La tabla de Costos no se renderizó correctamente."
+        allure.attach(driver.get_screenshot_as_png(), name="Tabla_Costos_OK", attachment_type=allure.attachment_type.PNG)
